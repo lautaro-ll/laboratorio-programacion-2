f117e0ca34fb19a1aa4aadc946d32af6db96cd42
Lautaro LL <62777637+lautaro-ll@users.noreply.github.com>
2020-09-28 08:42:48 -0300

    Ejercicio Integrador Agregado

---
+++ Ejercicio_Integrador_Clase_05/Biblioteca/Estante.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Estante
+    {
+        private Producto[] Producto;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            Producto = new Producto[capacidad];
+        }
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return this.Producto;
+        }
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            string codigoDeBarra;
+            foreach (Producto x in e.Producto)
+            {
+                stringBuilder.AppendLine (Biblioteca.Producto.MostrarProducto(x));
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            string codigo = (string) p;
+            bool flag = false;
+            foreach (Producto x in e.Producto)
+            {
+                if (!object.ReferenceEquals(x, null))
+                {
+                    string codigoEstante = (string)x;
+                    if (p.GetMarca() == x.GetMarca() && codigo == codigoEstante)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+
+            return flag;
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool flag = false;
+            if (e != p)
+            {
+                for(int i=0; i<e.Producto.Length; i++)
+                {
+                    if(object.ReferenceEquals(e.Producto[i], null)) //devuelve true si cuando compara hay un null
+                    {
+                        e.Producto[i] = p;
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            return flag;
+        }
+        public static Estante operator -(Estante e, Producto p)
+        {
+            Estante nuevoEstante = new Estante(e.Producto.Length);
+            bool flag = true;
+            if(e == p)
+            {
+                for (int i = 0; i < e.Producto.Length; i++)
+                {
+                    if(e.Producto[i] == p && flag)
+                    {
+                        flag = false;
+                    }
+                    else
+                    {
+                        nuevoEstante.Producto[i] = e.Producto[i];
+                    }
+                }
+            }
+            return nuevoEstante;
+
+        }
+    }
+}
---
+++ Ejercicio_Integrador_Clase_05/Biblioteca/Producto.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigo;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        public static string MostrarProducto(Producto p)
+        {
+            if (object.ReferenceEquals(p, null))
+            {
+                return "Vacío";
+            }
+            return $"Marca: {p.marca} - Precio: {p.precio} - Código de barras: {p.codigoDeBarra}";
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            if (object.ReferenceEquals(p, null))
+            {
+                return "";
+            }
+            return p.codigoDeBarra;
+        }
+        public static bool operator ==(Producto p, string marca)
+        {
+            if (object.ReferenceEquals(p, null))
+            {
+                return false;
+            }
+            return (p.marca == marca);
+        }
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            if (object.ReferenceEquals(p1, null) || object.ReferenceEquals(p2, null))
+            {
+                return false;
+            }
+            return (p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra);
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+    }
+}
