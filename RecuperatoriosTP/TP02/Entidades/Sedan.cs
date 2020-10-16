a6d65f5bfb93a4db7e21039ec8fe1ab00f420c37
Lautaro LL <62777637+lautaro-ll@users.noreply.github.com>
2020-10-16 18:40:01 -0300

    Recuperatorio TP2 Agregado

---
+++ RecuperatoriosTP/TP02/Entidades/Sedan.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        public enum ETipo { CuatroPuertas, CincoPuertas }
+        private ETipo tipo;
+
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo)
+            : base(chasis, marca, color)
+        {
+            this.tipo = tipo;
+        }
+        /// <summary>
+        /// Por defecto, TIPO será Monovolumen(CuatroPuertas)
+        /// </summary>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color)
+            : this(marca, chasis, color, ETipo.CuatroPuertas)
+        {
+        }
+        /// <summary>
+        /// Los automoviles son medianos
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+        /// <summary>
+        /// Publica tipo y tamaño.
+        /// </summary>
+        /// <returns>String con detalle de tipo y tamaño</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
